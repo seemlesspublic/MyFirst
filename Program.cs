919219f112d8aaf009d14bc2f994c7a3dfbec993
seemlesspublic <97595582+seemlesspublic@users.noreply.github.com>
2022-01-12 11:35:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ROMB
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+
+            {
+                for (int j = 10; j > i; j--)
+
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+
+                {
+                    Console.Write("*");
+
+                }
+
+
+
+                for (int j = 0; j <= i; j++)
+
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(" ");
+
+            }
+
+            // половина
+
+            for (int i = 10; i >= 0; i--)
+
+            {
+                for (int j = 10; j > i; j--)
+
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j <= i; j++)
+
+                {
+                    Console.Write("*");
+
+                }
+
+                for (int j = 0; j < i; j++)
+
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine(" ");
+
+            }
+        }
+    }
+}
